2401ca31299d26808134737a52a68fae65b89c2a
Andrey Okhotnikov <an.okhotnikov@gmail.com>
2019-03-20 11:25:17 +0300

    add graphql

--- pages/_app.js
+++ pages/_app.js
@@ -2,27 +2,33 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import App, { Container } from 'next/app';
 import withRedux from 'next-redux-wrapper';
+import { ApolloProvider } from 'react-apollo';
 import initStore from '../libs/store';
+import withApollo from '../libs/withApollo';
 
-export default withRedux(initStore)(
-  class MyApp extends App {
-    static async getInitialProps({ Component, ctx }) {
-      return {
-        pageProps: Component.getInitialProps
-          ? await Component.getInitialProps(ctx)
-          : {}
-      };
-    }
+export default withApollo(
+  withRedux(initStore)(
+    class MyApp extends App {
+      static async getInitialProps({ Component, ctx }) {
+        return {
+          pageProps: Component.getInitialProps
+            ? await Component.getInitialProps(ctx)
+            : {}
+        };
+      }
 
-    render() {
-      const { Component, pageProps, store } = this.props;
-      return (
-        <Container>
-          <Provider store={store}>
-            <Component {...pageProps} />
-          </Provider>
-        </Container>
-      );
+      render() {
+        const { Component, pageProps, store, apolloClient } = this.props;
+        return (
+          <ApolloProvider client={apolloClient}>
+            <Container>
+              <Provider store={store}>
+                <Component {...pageProps} />
+              </Provider>
+            </Container>
+          </ApolloProvider>
+        );
+      }
     }
-  }
+  )
 );
--- pages/a.js
+++ pages/a.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
 
-const A = ({ login }) => {
-  return <div>a</div>;
+const A = ({ login, data: { loading, hi } }) => {
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  return <div>{hi}</div>;
 };
 
 const mapStateToProps = state => ({
   login: state.me.login
 });
 
+const hiQuery = gql`
+  {
+    hi
+  }
+`;
+
 const mapDispatchToProps = {};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+export default compose(
+  graphql(hiQuery),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
 )(A);
